ed30b9ad79930abb1ddbd7a2f2ddbdf266f627be
Sören Busch <sbusch@mobivention.com>
2015-07-13 16:09:55 +0200

    init

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+use_frameworks!
+
+target 'TestPod' do
+
+pod 'FBSDKCoreKit'
+pod 'FBSDKLoginKit'
+pod 'FBSDKShareKit'
+
+end
+
+target 'TestPodTests' do
+
+end
+
---
+++ TestPod/TestClass.swift
@@ -0,0 +1,19 @@
+//
+//  TestClass.swift
+//  TestPod
+//
+//  Created by Sören Busch on 13.07.15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+import Foundation
+import FBSDKCoreKit
+
+class TestClass: NSObject {
+	
+	static func doStuff()
+	{
+		FBSDKUtility.dictionaryWithQueryString("lol")
+	}
+	
+}
